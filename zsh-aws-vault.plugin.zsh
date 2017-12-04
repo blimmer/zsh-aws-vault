d3de5325e9d30fef172c3eb160a8d257ae9e603c
Nick Huanca <nick.huanca@ibotta.com>
2017-12-04 09:41:37 -0700

    Added more helpers

--- zsh-aws-vault.plugin.zsh
+++ zsh-aws-vault.plugin.zsh
@@ -13,6 +13,35 @@ function avsh() {
   aws-vault exec $1 -- zsh
 }
 
+function aws-login() {
+    aws-vault login $1
+}
+
+function aws-login-incognito() {
+    aws-get-login-link $1 | xargs /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --incognito --new-window
+}
+
+function aws-get-login-link() {
+    aws-vault login -s $1
+}
+
+function aws-profiles() {
+    local -a profiles
+    local _profile_text _role
+    if egrep -arn "^\[default\]" ~/.aws/config >/dev/null; then
+        profiles+="default: IAM_Keys"
+    fi
+    for item in $(grep "\[profile " ~/.aws/config | sed -e 's/.*profile \([a-zA-Z0-9_-]*\).*/\1/' | sort); do
+        _profile_text="$item: "
+        _role=$(aws --profile $item configure get role_arn)
+        if [ "$_role" != "" ]; then
+            _profile_text+="ROLE($_role) "
+        fi
+        profiles+=$_profile_text
+    done
+    printf '%s\n' "${profiles[@]}" | column -t
+}
+
 #--------------------------------------------------------------------#
 # Prompt Customization                                               #
 #--------------------------------------------------------------------#
