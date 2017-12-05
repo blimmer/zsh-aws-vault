ac44f2269f0e067fff65ea8cdac5a39f13c68655
Ben Limmer <ben@benlimmer.com>
2017-12-04 18:46:58 -0700

    test stubs

--- test/aliases.sh
+++ test/aliases.sh
@@ -4,3 +4,12 @@
   result="$(echo 2+2 | bc)"
   [ "$result" -eq 4 ]
 }
+
+@test "stubs" {
+  stub date \
+    "${_DATE_ARGS} : echo 1460967598.184561556" \
+
+    run get_timestamp
+    assert_success
+    assert_output 1460967598184
+}
---
+++ test/test_helper.bash
@@ -0,0 +1,1 @@
+load helpers/mocks/stub
